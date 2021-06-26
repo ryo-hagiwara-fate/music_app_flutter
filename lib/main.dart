a54af7cede6b1dc8f592d2fa542ebd0b4ffc3276
萩原涼【マーケティング】(ryo hagiwara) <ryo_hagiwara@mates-system.com>
2021-06-26 16:47:20 +0900

    add: my changes

--- lib/main.dart
+++ lib/main.dart
@@ -4,18 +4,19 @@ import 'package:audioplayers/audio_cache.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
-  void playSound(int soundNumber) {
+
+  void playSound(int soundNumber){
     final player = AudioCache();
     player.play('note$soundNumber.wav');
   }
 
-  Expanded buildKey({Color color, int soundNumber}) {
+  buildKey(Color color,int soundNumber){
     return Expanded(
       child: FlatButton(
-        color: color,
-        onPressed: () {
+        onPressed: (){
           playSound(soundNumber);
         },
+        color: color,
       ),
     );
   }
@@ -24,20 +25,19 @@ class XylophoneApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.black,
         body: SafeArea(
           child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                buildKey(color: Colors.red, soundNumber: 1),
-                buildKey(color: Colors.orange, soundNumber: 2),
-                buildKey(color: Colors.yellow, soundNumber: 3),
-                buildKey(color: Colors.green, soundNumber: 4),
-                buildKey(color: Colors.teal, soundNumber: 5),
-                buildKey(color: Colors.blue, soundNumber: 6),
-                buildKey(color: Colors.purple, soundNumber: 7),
-              ],
-          ),
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(Colors.red,1),
+              buildKey(Colors.orange,2),
+              buildKey(Colors.yellow,3),
+              buildKey(Colors.green,4),
+              buildKey(Colors.teal,5),
+              buildKey(Colors.blue,6),
+              buildKey(Colors.purple,7)
+            ],
+          )
         ),
       ),
     );
